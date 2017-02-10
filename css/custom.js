a15141c457e7990c07f358af63bda4e5635f032f
Mason Embry <mason@embrycode.com>
2017-02-10 17:25:31 -0600

    Improving recaptcha.

---
+++ css/custom.js
@@ -0,0 +1,8 @@
+$("form").submit(function(event) {
+
+   var recaptcha = $("#g-recaptcha-response").val();
+   if (recaptcha === "") {
+      event.preventDefault();
+      alert("Please check the recaptcha");
+   }
+});
